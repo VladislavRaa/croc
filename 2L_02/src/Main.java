14f4cbf60d03fe55c7a9f1867e4dcd5027396e0e
Vladislav Raa <vladras97@yandex.ru>
2019-03-10 20:29:51 +0300

    gitignore dislike

--- 2L_02/src/Main.java
+++ 2L_02/src/Main.java
@@ -3,4 +3,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public class A {
+
+    }
+
+    public class B extends A{
+
+    }
+
+    public class C extends B {
+
+    }
 }
