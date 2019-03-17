13e33b0eebf461a71ee7ece4ccce182ac6fbd576
Vladislav Raa <vladras97@yandex.ru>
2019-03-17 20:16:55 +0300

    final

--- 3L_01/src/Chessboard.java
+++ 3L_01/src/Chessboard.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Chessboard {
     private int x;
     private int y;
-    private static final int ASCII_CONST_LETTERS = 98;
+    private static final int ASCII_CONST_LETTERS = 97;
     private static final int ASCII_CONST_NUMBERS = 49;
 
     public Chessboard(int x, int y) {
@@ -19,7 +19,7 @@ public class Chessboard {
     }
 
     public int[] getXY() {
-        return new int[]{this.x, this.y};
+        return new int[] {this.x, this.y};
     }
 
     private void check(int x, int y) {
@@ -28,66 +28,60 @@ public class Chessboard {
         }
     }
 
-    public int[] convertToInt(String array) {
+    private int[] convertToInt(String array) throws MyExceptionClass {
         if (array.length() != 2) {
-            throw new IllegalArgumentException("lenght != 2");
+            throw new MyExceptionClass("lenght != 2");
         }
         int oX = array.charAt(0) - ASCII_CONST_LETTERS;
         int oY = array.charAt(1) - ASCII_CONST_NUMBERS;
+
         check(oX, oY);
-        return new int[] {oX + 1, oY + 1};
+        return new int[] {oX, oY};
     }
 
     public String convertToStr(int x, int y) {
-        return (char)(x + ASCII_CONST_LETTERS) + "" + (char)(y + ASCII_CONST_NUMBERS );
+        return (char) (x + ASCII_CONST_LETTERS) + "" + (char) (y + ASCII_CONST_NUMBERS);
     }
 
-    public void print(String[] str) {
-        for (String i: str) {
+    public String convertToStr() {
+        return (char) (x + ASCII_CONST_LETTERS) + "" + (char) (y + ASCII_CONST_NUMBERS);
+    }
+
+    private void print(String[] str) {
+        for (String i : str) {
             System.out.println(i);
         }
     }
 
-    public void getCheckArray(String[] array) {
-        print(array);
-
-        int startX = convertToInt(array[0])[0];
-        int startY =convertToInt(array[0])[1];
-
-
-        for (int i = 1; i < array.length; i++ ) {
-            //System.out.println(Math.abs(convert(array[i])[0] - startX) + " " + Math.abs(convert(array[i])[1] - startY));
-
-            if((Math.abs(convertToInt(array[i])[0] - startX) == 1) && (Math.abs(convertToInt(array[i])[1] - startY) == 2 )) {
-                System.out.print(startX + "" + startY + "->" + convertToInt(array[i])[0] + "" + convertToInt(array[i])[1] + " ");
-                //System.out.print(convertToStr(startX, startX) + "->" + convertToStr(convertToInt(array[i])[0],convertToInt(array[i])[1]) + " ");
-                startX = convertToInt(array[i])[0];
-                startY = convertToInt(array[i])[1];
-            }
-
-            if((Math.abs(convertToInt(array[i])[0] - startX) == 2) && (Math.abs(convertToInt(array[i])[1]  - startY) == 1)) {
-                System.out.print(startX + "" + startY + "->" + convertToInt(array[i])[0] + "" + convertToInt(array[i])[1] + " ");
-                //System.out.print(convertToStr(startX, startX) + "->" + convertToStr(convertToInt(array[i])[0],convertToInt(array[i])[1]) + " ");
-                startX = convertToInt(array[i])[0];
-                startY = convertToInt(array[i])[1];
-            }
+    private boolean checkFromTo(String from, String to) throws MyExceptionClass{
+        if ((Math.abs(convertToInt(to)[0] - convertToInt(from)[0]) == 1) && (Math.abs(convertToInt(to)[1] - convertToInt(from)[1]) == 2)) {
+            return true;
         }
+        if ((Math.abs(convertToInt(to)[0] - convertToInt(from)[0]) == 2) && (Math.abs(convertToInt(to)[1] - convertToInt(from)[1]) == 1)) {
+            return true;
+        }
+        throw new MyExceptionClass("конь так не ходит: " + from + " -> " + to);
     }
 
+    public void checkArray(String[] array) {
+        try {
+            for (int i = 0; i < array.length - 1; i++) {
+                checkFromTo(array[i], array[i + 1]);
+            }
+            System.out.println("OK");
+        } catch (MyExceptionClass e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
     @Override
     public String toString() {
         return Character.toString((char) (ASCII_CONST_LETTERS + this.x)) + (this.y + 1);
     }
 
-
-
-
-    class MyExceptionClass extends Throwable {
-        private String result;
-
-        public MyExceptionClass(String message, String result) {
+    class MyExceptionClass extends Exception {
+        public MyExceptionClass(String message) {
             super(message);
-            this.result = result;
         }
     }
 }
--- 3L_01/src/Main.java
+++ 3L_01/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Chessboard test = new Chessboard(4,2);
-        String[] mas = {"g8", "e7", "c8"};
-        test.getCheckArray(mas);
+    public static void main(String[] args) throws Chessboard.MyExceptionClass {
+        Chessboard test = new Chessboard(1,1);
+        //System.out.println(test.toString());
+        String[] mas = {"g3", "a3", "c4", "a1"};
+        String[] mas1 = {"g8", "e7", "e6"};
+        String[] mas2 = {"g8", "e7", "c8"};
+        //test.checkArray(mas);
+        //test.checkArray(mas1);
+        test.checkArray(mas2);
         //System.out.println(test.toString());
     }
 }
