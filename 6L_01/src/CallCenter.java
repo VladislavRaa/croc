6789f0a58241b5134929c76a04484f962f20b893
Vladislav Raa <vladras97@yandex.ru>
2019-04-06 13:52:33 +0300

    gotovo

--- 6L_01/src/CallCenter.java
+++ 6L_01/src/CallCenter.java
@@ -1,58 +1,18 @@
 import java.util.*;
 
-public class CallCenter{
-
-
-    public void countLog(ArrayList<String> log) { //1,2 oper
-        String[] tempString;
-        Integer tempTotalTime;
-        Integer greatestTotalTime = 0;
-        String[] tempTime ;
-        HashMap<String, String[]> operators = new HashMap<>();//operator 1,2
-
+public class CallCenter {
+    public void countLog(ArrayList<String> log, int value) {
+        ArrayList<String[]> operators = new ArrayList<>();
         for (String operator : log) {
-            tempString = operator.split(",");
-            greatestTotalTime = Integer.valueOf(tempString[1]) - Integer.valueOf(tempString[0]);
-            if (operators.containsKey(tempString[2])) {
-                tempTime = operators.get(tempString[2]);
-                tempTotalTime = Integer.valueOf(tempTime[1]) - Integer.valueOf(tempTime[0]);
-
-                if (tempTotalTime < greatestTotalTime) {
-                    operators.put(tempString[2], new String[]{tempString[0], tempString[1], String.valueOf(Integer.valueOf(tempString[1]) - Integer.valueOf(tempString[0]))});
-                }
-            } else {
-                operators.put(tempString[2], new String[]{tempString[0], tempString[1], String.valueOf(Integer.valueOf(tempString[1]) - Integer.valueOf(tempString[0]))});
-            }
+            operators.add(operator.split(","));
         }
-        ArrayList<String[]> byTotalTime = new ArrayList<>(operators.values());
-
-        //byTotalTime.sort(Collections.reverseOrder());
-       /* byTotalTime.sort((obj1, obj2) -> {
-            int one = Integer.parseInt(obj1[0]);
-            int two = Integer.parseInt(obj2[0]);
-            if(one > two) {
-                return one;
-            }else {
-                return two;
-            }
-        });*/
-        byTotalTime.sort((o1, o2) -> {
-            int one = Integer.parseInt(o1[2]);
-            int two = Integer.parseInt(o2[2]);
+        operators.sort((o1, o2) -> {
+            int one = Integer.valueOf(o1[1]) - Integer.valueOf(o1[0]);
+            int two = Integer.valueOf(o2[1]) - Integer.valueOf(o2[0]);
             return Integer.compare(two, one);
-
         });
-        for (int i = 0; i<3; i++){
-            System.out.println(getKeyFromValue(operators, byTotalTime.get(i)));
+        for (int i = 0; i < value; i++) {
+            System.out.println(operators.get(i)[0] + ", " + operators.get(i)[1] + ", " + operators.get(i)[2]);
         }
-    }
-
-   private Object getKeyFromValue(Map hm, Object value) {
-        for (Object o : hm.keySet()) {
-            if (hm.get(o).equals(value)) {
-                return o;
-            }
-        }
-        return null;
     }
 }
--- 6L_01/src/Main.java
+++ 6L_01/src/Main.java
@@ -1,19 +1,14 @@
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
         ArrayList<String> log = new ArrayList<String>();
-        log.add("1,121,operator1");
-        log.add("4,13,operator2");
+        log.add("1,12,operator1");
+        log.add("4,6,operator2");
         log.add("7,12,operator2");
         log.add("10,32,operator3");
         log.add("15,30,operator1");
-        log.add("10,31,operator4");
-        log.add("10,111,operator4");
-        log.add("10,30,operator1");
-
         CallCenter cc = new CallCenter();
-        cc.countLog(log);
+        cc.countLog(log, 3);
     }
 }
